c4e9f0358780ab8b559abf94631f39012bb35adc
seungyeop12 <art7449@gmail.com>
2023-09-20 15:44:40 +0900

    년도, 달, 일, 요일, 시간, 분, 초

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
-const box = document.querySelector('article');
-console.log(box);
+const now = new Date();
+console.dir(now);
+
+const months = [
+	'Januaty',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'August',
+	'Semtember',
+	'October',
+	'November',
+	'December',
+];
+
+const days = ['Sunday', 'monday', 'Tuesday', 'wednesday', 'Thuesday', 'Friday', 'Saturday'];
+
+const year = now.getFullYear();
+const month = now.getMonth();
+const date = now.getDate();
+const day = now.getDay();
+const hours = now.getHours();
+const min = now.getMinutes();
+const sec = now.getSeconds();
+console.log(sec);
