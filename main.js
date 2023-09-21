927625939e5748ad5e191b5b63cd9bab757eab0a
seungyeop12 <art7449@gmail.com>
2023-09-21 09:33:14 +0900

    if

--- main.js
+++ main.js
@@ -1,37 +1,34 @@
 const now = new Date();
-console.dir(now);
-
-const months = [
-	'Januaty',
-	'February',
-	'March',
-	'April',
-	'May',
-	'June',
-	'August',
-	'Semtember',
-	'October',
-	'November',
-	'December',
-];
-
-const days = ['Sunday', 'monday', 'Tuesday', 'wednesday', 'Thuesday', 'Friday', 'Saturday'];
-
-const year = now.getFullYear();
-const month = now.getMonth();
-const date = now.getDate();
-const day = now.getDay();
 const hours = now.getHours();
 const min = now.getMinutes();
 const sec = now.getSeconds();
 
-console.log(hours);
+// let newHr = 0;
 
-let newHr = 0;
+// if (hours > 12) {
+// 	newHr = hours - 12 + 'pm';
+// 	if (newHr < 10) {
+// 		newHr = '0' + newHr + 'pm';
+// 	} else {
+// 		newHr = newHr = 'pm';
+// 	}
+// } else {
+// 	newHr = hours;
 
-if (hours > 12) {
-	newHr = hours - 12 + 'pm';
-} else {
-	newHr = hours + 'am';
+// 	if (newHr < 10) {
+// 		newHr = '0' + newHr + 'am';
+// 	} else {
+// 		newHr = newHr + 'am';
+// 	}
+// }
+
+console.log(setNumbers(hours));
+
+function setNumbers(num) {
+	if (num < 10) {
+		result = '0' + num;
+	} else {
+		result = num;
+	}
+	return result;
 }
-console.log(newHr);
