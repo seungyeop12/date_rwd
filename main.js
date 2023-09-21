a7bc29d5a743b2281c2b490055404e32461095a4
seungyeop12 <art7449@gmail.com>
2023-09-21 10:17:25 +0900

    시간값 가공

--- main.js
+++ main.js
@@ -1,48 +1,20 @@
+const [hr, m, s] = document.querySelectorAll('h1 span');
+
 const now = new Date();
 const hours = now.getHours();
 const min = now.getMinutes();
 const sec = now.getSeconds();
 
-// let newHr = 0;
-
-// if (hours > 12) {
-// 	newHr = hours - 12 + 'pm';
-// 	if (newHr < 10) {
-// 		newHr = '0' + newHr + 'pm';
-// 	} else {
-// 		newHr = newHr = 'pm';
-// 	}
-// } else {
-// 	newHr = hours;
-
-// 	if (newHr < 10) {
-// 		newHr = '0' + newHr + 'am';
-// 	} else {
-// 		newHr = newHr + 'am';
-// 	}
-// }
-
-// console.log(setNumbers(hours));
-
-// function setNumbers(num) {
-// 	if (num < 10) {
-// 		result = '0' + num;
-// 	} else {
-// 		result = num;
-// 	}
-// 	return result;
-// }
-console.log(setNumbers(hours));
+hr.innerText = setNumbers(setHours(hours));
+m.innerText = setNumbers(min);
+s.innerText = setNumbers(sec);
 
 function setNumbers(num) {
-	/*
-	if (num < 10) num = '0' + num;	
-	*/
-	//조건식 ? true일때 실행할 구문 : false일때 실행할 구문
-	//삼항연산자 구문안에는 대입연산자가 들어올수 없기 때문에 괄호로 묶어서 표현식으로 변경
-	//특정값이 else 일때 굳이 실행할 코드가 없다면 &&연산자 활용
-	//num < 10 ? (result = '0' + num) : (result = num);
 	num < 10 && (num = '0' + num);
+	return num;
+}
 
+function setHours(num) {
+	num > 12 && (num = num - 12);
 	return num;
 }
